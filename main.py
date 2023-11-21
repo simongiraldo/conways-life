a597fa4287e70b1efa9ee17db95f4844495ef8c7
Simon Giraldo <simongiraldoc@gmail.com>
2023-11-21 01:16:53 -0500

    Able to paint onClick square, add pause menu

--- main.py
+++ main.py
@@ -26,12 +26,47 @@ class Square():
     def die(self):
         self.width = 1
 
+class Pause(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./sprites/pause.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = X_COORD_PAUSE
+        self.rect.y = Y_COORD_PAUSE
+
+class Restart(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./sprites/restart.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = X_COORD_RESTART
+        self.rect.y = Y_COORD_RESTART
+
+class Music(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./sprites/music.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = X_COORD_MUSIC
+        self.rect.y = Y_COORD_MUSIC
+
 
 class Game(object):
     def __init__(self):
         self.playing = True 
         self.squares = []
         self.areas = {}
+        self.game_paused = False
+        self.paused_screen = pygame.Surface(SCREEN_SIZE, pygame.SRCALPHA)
+        self.pause_button = Pause()
+        self.restart_button = Restart()
+        self.music_button = Music()
+        self.pause_sprites = pygame.sprite.Group()
+
+        self.pause_sprites.add(self.pause_button, self.restart_button, self.music_button)
 
         x_coord = 0
         y_coord = 0
@@ -55,31 +90,65 @@ class Game(object):
             if event.type == pygame.QUIT:
                 self.playing = False
 
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                x_mouse, y_mouse = event.pos
-                mouse_position = (x_mouse, y_mouse)
-                for area, square in self.areas.items():
-                    if pygame.Rect(area, (SQUARE_WIDTH, SQUARE_HEIGHT)).collidepoint(mouse_position):
-                        square.update()
-                        break
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not self.game_paused:
+                square_coords = self.get_square_coord(event.pos)
+                self.areas[square_coords].update()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.game_paused:
+                self.game_paused = not self.game_paused
+
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.game_paused = not self.game_paused
+                print("Space pressed")
+                
     
     def run_logic(self):
-        pass
+        if self.game_paused:
+            return
 
     def display_frame(self, screen):
         screen.fill(BACKGROUND_COLOR)
-
+        
         for square in self.squares:
             pygame.draw.rect(screen, WHITE_SMOKE, square.shape, width=square.width)
 
+        if self.game_paused:
+            self.paused_screen.fill(WHITE_BLURRED)
+            screen.blit(self.paused_screen, FIRST_COORDS)
+            self.pause_sprites.draw(screen)
+            print("Showing pause screen")
+        
         display.flip()
 
+
+    def get_square_coord(self, coords):
+        x_mouse = coords[0]
+        y_mouse = coords[1]
+
+        x_coord = 0
+        y_coord = 0
+
+        if x_mouse <= SQUARE_WIDTH and y_mouse <= SQUARE_HEIGHT:
+            return FIRST_COORDS
+        
+        if x_mouse % SQUARE_WIDTH == 0:
+            x_coord = ((x_mouse // SQUARE_WIDTH) - 1) * SQUARE_WIDTH
+        else:
+            x_coord = (x_mouse // SQUARE_WIDTH) * SQUARE_WIDTH
+
+        if y_mouse % SQUARE_HEIGHT == 0:
+            y_coord = ((y_mouse // SQUARE_HEIGHT) - 1) * SQUARE_HEIGHT
+        else:
+            y_coord = (y_mouse // SQUARE_HEIGHT) * SQUARE_HEIGHT
+
+        return (x_coord, y_coord)
 
 def run():
     pygame.init()
     display.init()
 
-    screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption("Conway's life game")
 
     clock = pygame.time.Clock()
 
@@ -99,3 +168,11 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+
+# Añadir menu de inicio, tal vez con un enlance a una pagina que explique de que trata el juego
+# Hacer que el click en la imagen de play, se reinicie el juego
+# Hacer que al click en restart, se limpie todo(self.__init__())
+# 
+# (El sonido puede ser una feature para despues)
+# 
--- main2.py
+++
@@ -1,6 +0,0 @@
-some  = {}
-area = (5, 5)
-some[area] = 5
-
-for i, j in some:
-    print(i)
