c28d19c1818cf54fcbc9a511cb624abc997054c1
Simon Giraldo <simongiraldoc@gmail.com>
2023-12-08 22:09:14 -0500

    TODOs deleted

--- main.py
+++ main.py
@@ -28,15 +28,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-
-
-# TODO:
-# Actalizar el readme con link a paginas con info sobre el juego y poner la info en general del proyeco
-# Crear subfunciones para hacer el codigo mas legible y menos spaguetthi
-# Separar cada clase en archivos diferentes?
-# Hacer que al darle pause, poder parar la ejecucion y ver bien el patron, sin tener que mostar el menu de pause (Tal vez mover los botones en una parte de abajo del grid)
-# Hacer una opcion de grid dinamico, mientras se ejecuten las generaciones, puedo vivir o matar cuadros. Harian parte de la generacion despues de 2 segundos de haber hecho click 
-# Hacer que el tablero sea infinito, al pasar por el lado derecho, que vuelva por l izquierdo
-# 
-# (El sonido puede ser una feature para despues)
-# 
